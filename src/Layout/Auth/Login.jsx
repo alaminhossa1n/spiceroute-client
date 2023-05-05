daa9f49c2fc6a7af34a6b7cab266129bd0eb6bb5
alaminhossa1n <alaminatghoria@gmail.com>
2023-05-05 22:04:57 +0600

    login field requered

--- src/Layout/Auth/Login.jsx
+++ src/Layout/Auth/Login.jsx
@@ -87,6 +87,7 @@ const Login = () => {
                             type="email"
                             className="block w-full px-4 py-2 mt-2 text-purple-700 bg-white border rounded-md focus:border-purple-400 focus:ring-purple-300 focus:outline-none focus:ring focus:ring-opacity-40"
                             name='email'
+                            required
                         />
                     </div>
                     <div className="mb-2">
@@ -100,6 +101,7 @@ const Login = () => {
                             type="password"
                             className="block w-full px-4 py-2 mt-2 text-purple-700 bg-white border rounded-md focus:border-purple-400 focus:ring-purple-300 focus:outline-none focus:ring focus:ring-opacity-40"
                             name='password'
+                            required
                         />
                     </div>
 
